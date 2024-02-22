fded16099a80ff2559c776a090d32873bf5bb466
John Perrin <john.d.perrin@gmail.com>
2024-02-22 22:56:33 +0000

    Unit 1 - Lesson 6

--- src/Unit-1/DoThis/TailActor.cs
+++ src/Unit-1/DoThis/TailActor.cs
@@ -3,10 +3,13 @@
 namespace WinTail;
 
 /// <summary>
-/// Monitors the file at <see cref="_filePath"/> for changes and sends
+/// Monitors the file at <see>
+///     <cref>filePath</cref>
+/// </see>
+/// for changes and sends
 /// file updates to console.
 /// </summary>
-public class TailActor : UntypedActor
+public class TailActor(IActorRef reporterActor, string filePath) : UntypedActor
 {
     #region Message types
 
@@ -57,55 +60,59 @@ public class TailActor : UntypedActor
 
     #endregion
 
-    private readonly string _filePath;
-    private readonly IActorRef _reporterActor;
-    private readonly FileObserver _observer;
-    private readonly Stream _fileStream;
-    private readonly StreamReader _fileStreamReader;
+    private FileObserver _observer;
+    private Stream _fileStream;
+    private StreamReader _fileStreamReader;
 
-    public TailActor(IActorRef reporterActor, string filePath)
+    protected override void PreStart()
     {
-        _reporterActor = reporterActor;
-        _filePath = filePath;
-
         // start watching file for changes
-        _observer = new FileObserver(Self, Path.GetFullPath(_filePath));
+        _observer = new FileObserver(Self, Path.GetFullPath(filePath));
         _observer.Start();
 
         // open the file stream with shared read/write permissions
         // (so file can be written to while open)
-        _fileStream = new FileStream(Path.GetFullPath(_filePath),
+        _fileStream = new FileStream(Path.GetFullPath(filePath),
             FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
         _fileStreamReader = new StreamReader(_fileStream, Encoding.UTF8);
 
         // read the initial contents of the file and send it to console as first msg
         var text = _fileStreamReader.ReadToEnd();
-        Self.Tell(new InitialRead(_filePath, text));
+        Self.Tell(new InitialRead(filePath, text));
+    }
+
+    protected override void PostStop()
+    {
+        _observer.Dispose();
+        _observer = null;
+        _fileStreamReader.Close();
+        _fileStreamReader.Dispose();
+        base.PostStop();
     }
 
     protected override void OnReceive(object message)
     {
-        if (message is FileWrite)
+        switch (message)
         {
-            // move file cursor forward
-            // pull results from cursor to end of file and write to output
-            // (this is assuming a log file type format that is append-only)
-            var text = _fileStreamReader.ReadToEnd();
-            if (!string.IsNullOrEmpty(text))
+            case FileWrite:
             {
-                _reporterActor.Tell(text);
-            }
+                // move file cursor forward
+                // pull results from cursor to end of file and write to output
+                // (this is assuming a log file type format that is append-only)
+                var text = _fileStreamReader.ReadToEnd();
+                if (!string.IsNullOrEmpty(text))
+                {
+                    reporterActor.Tell(text);
+                }
 
-        }
-        else if (message is FileError)
-        {
-            var fe = message as FileError;
-            _reporterActor.Tell(string.Format("Tail error: {0}", fe.Reason));
-        }
-        else if (message is InitialRead)
-        {
-            var ir = message as InitialRead;
-            _reporterActor.Tell(ir.Text);
+                break;
+            }
+            case FileError fe:
+                reporterActor.Tell($"Tail error: {fe.Reason}");
+                break;
+            case InitialRead ir:
+                reporterActor.Tell(ir.Text);
+                break;
         }
     }
 }
